d144a907d69c3cb89ef6f7f2c9b26b3c1e5d35a5
jackeline varela vasquez <jackelinevarela2@gmail.com>
2018-10-18 12:19:12 -0600

    Parte del examen

--- Examen_1P/Examen_1P/Form1.Designer.cs
+++ Examen_1P/Examen_1P/Form1.Designer.cs
@@ -28,150 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblVal = new System.Windows.Forms.Label();
+            this.lblComent = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblinstruc = new System.Windows.Forms.Label();
+            this.rtxtComentarios = new System.Windows.Forms.RichTextBox();
+            this.dGVProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVProduct)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // lblNombre
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(28, 28);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 284);
-            this.dataGridView1.TabIndex = 0;
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(311, 62);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(91, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre Completo";
             // 
-            // label1
+            // lblCorreo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(309, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(311, 111);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(94, 13);
+            this.lblCorreo.TabIndex = 2;
+            this.lblCorreo.Text = "Correo Electrónico";
             // 
-            // label2
+            // lblVal
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(309, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.lblVal.AutoSize = true;
+            this.lblVal.Location = new System.Drawing.Point(310, 161);
+            this.lblVal.Name = "lblVal";
+            this.lblVal.Size = new System.Drawing.Size(57, 13);
+            this.lblVal.TabIndex = 3;
+            this.lblVal.Text = "Valoración";
             // 
-            // label3
+            // lblComent
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(309, 129);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.lblComent.AutoSize = true;
+            this.lblComent.Location = new System.Drawing.Point(310, 211);
+            this.lblComent.Name = "lblComent";
+            this.lblComent.Size = new System.Drawing.Size(65, 13);
+            this.lblComent.TabIndex = 4;
+            this.lblComent.Text = "Comentarios";
             // 
-            // label4
+            // txtNombre
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(309, 180);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.txtNombre.Location = new System.Drawing.Point(314, 78);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(241, 20);
+            this.txtNombre.TabIndex = 5;
             // 
-            // textBox1
+            // txtCorreo
             // 
-            this.textBox1.Location = new System.Drawing.Point(312, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(241, 20);
-            this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(313, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(241, 20);
-            this.textBox2.TabIndex = 6;
+            this.txtCorreo.Location = new System.Drawing.Point(314, 127);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(241, 20);
+            this.txtCorreo.TabIndex = 6;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(312, 145);
+            this.textBox3.Location = new System.Drawing.Point(312, 177);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(241, 20);
+            this.textBox3.Size = new System.Drawing.Size(243, 20);
             this.textBox3.TabIndex = 7;
             // 
-            // dataGridView2
+            // btnAceptar
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(312, 205);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(240, 107);
-            this.dataGridView2.TabIndex = 8;
+            this.btnAceptar.Location = new System.Drawing.Point(177, 350);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(133, 23);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
-            // button1
+            // btnSalir
             // 
-            this.button1.Location = new System.Drawing.Point(177, 329);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSalir.Location = new System.Drawing.Point(328, 350);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(133, 23);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // button2
+            // lblinstruc
             // 
-            this.button2.Location = new System.Drawing.Point(330, 329);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lblinstruc.AutoSize = true;
+            this.lblinstruc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblinstruc.Location = new System.Drawing.Point(105, 28);
+            this.lblinstruc.Name = "lblinstruc";
+            this.lblinstruc.Size = new System.Drawing.Size(404, 24);
+            this.lblinstruc.TabIndex = 11;
+            this.lblinstruc.Text = "Seleccione el produto a realizar reseña/análisis";
+            this.lblinstruc.UseWaitCursor = true;
+            // 
+            // rtxtComentarios
+            // 
+            this.rtxtComentarios.Location = new System.Drawing.Point(314, 238);
+            this.rtxtComentarios.Name = "rtxtComentarios";
+            this.rtxtComentarios.Size = new System.Drawing.Size(241, 96);
+            this.rtxtComentarios.TabIndex = 12;
+            this.rtxtComentarios.Text = "";
+            // 
+            // dGVProduct
+            // 
+            this.dGVProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVProduct.Location = new System.Drawing.Point(12, 74);
+            this.dGVProduct.Name = "dGVProduct";
+            this.dGVProduct.Size = new System.Drawing.Size(278, 260);
+            this.dGVProduct.TabIndex = 13;
+            this.dGVProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGVProduct_CellContentClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 364);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView2);
+            this.ClientSize = new System.Drawing.Size(629, 385);
+            this.Controls.Add(this.dGVProduct);
+            this.Controls.Add(this.rtxtComentarios);
+            this.Controls.Add(this.lblinstruc);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblComent);
+            this.Controls.Add(this.lblVal);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblNombre);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVProduct)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblVal;
+        private System.Windows.Forms.Label lblComent;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCorreo;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblinstruc;
+        private System.Windows.Forms.RichTextBox rtxtComentarios;
+        private System.Windows.Forms.DataGridView dGVProduct;
     }
 }
 
--- Examen_1P/Examen_1P/Form1.cs
+++ Examen_1P/Examen_1P/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace Examen_1P
 {
     public partial class Form1 : Form
@@ -15,6 +17,56 @@ namespace Examen_1P
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Server = localhost; Database=AdventureWorks2014; User Id=Jacki; 
+                                    Password=12345;");
+            //Crear el query de sql server
+            string ins = "exec sp_ver_producto @id , @nombre , @mail , @rate , @comments";
+
+            //Crear el comando
+            SqlCommand cmd = new SqlCommand(ins, conn);
+
+            try
+            {
+                //abrir la coneccion
+                conn.Open();
+                //llenar el comando con los parametros
+                using (cmd)
+                {
+                    cmd.Parameters.Add("@id", SqlDbType.Int)
+                    cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombre.Text;
+                    cmd.Parameters.Add("@mail", SqlDbType.VarChar).Value = txtCorreo.Text;
+                    cmd.Parameters.Add("@rate", SqlDbType.Int);
+                    cmd.Parameters.Add("@comments", SqlDbType.VarChar);
+                }
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, "Detalles de excepcion!");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void dGVProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            // Cerrar la conexion
+            conn.Close();
         }
     }
 }
